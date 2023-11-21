be0fb43b509974c646b63d8a96ff40e7136ffabf
Aleshere <alessioburgio85@gmail.com>
2023-11-21 13:11:30 -0500

    tailwind setup

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen space-y-8 my-8 flex-col">
+      Home Page
+    </main>
+  )
+}
